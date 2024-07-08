a8a09b26a462f99072305cf5da966d6501c58ae3
softengineerstevens <softengineerstevens@gmail.com>
2024-07-08 12:03:56 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 50 + 1);
+
+const num3 = 16;
+const num4 = 6;
+const mod =num3%num4;
+
+const max = Math.max(20);
+console.log(max);
+
+
+
+
+
+
+
